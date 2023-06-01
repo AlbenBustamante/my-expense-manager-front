96f86c711a1759e62dfe1c96703b9e1ab6a8ce0b
Alben Bustamante <albenbustamante@gmail.com>
2023-06-01 17:04:57 -0500

    feature: create a provisional navbar

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -4,14 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FeatureRoutingModule } from './feature-routing.module';
 import { FeatureComponent } from './feature.component';
 
-
 @NgModule({
-  declarations: [
-    FeatureComponent
-  ],
-  imports: [
-    CommonModule,
-    FeatureRoutingModule
-  ]
+  declarations: [FeatureComponent],
+  imports: [CommonModule, FeatureRoutingModule],
 })
-export class FeatureModule { }
+export class FeatureModule {}
--- src/app/feature/home/home.module.ts
+++ src/app/feature/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { ReportsComponent } from './pages/reports/reports.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ReportsComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
