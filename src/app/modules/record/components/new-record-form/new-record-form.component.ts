910009a3697de80c814e202fa08379bab2a7febc
Alben Bustamante <albenbustamante@gmail.com>
2023-06-02 17:20:58 -0500

    add: category's section on navbar

--- src/app/modules/record/components/new-record-form/new-record-form.component.ts
+++ src/app/modules/record/components/new-record-form/new-record-form.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-record-form',
   templateUrl: './new-record-form.component.html',
-  styleUrls: ['./new-record-form.component.css']
+  styleUrls: ['./new-record-form.component.css'],
 })
-export class NewRecordFormComponent {
-
-}
+export class NewRecordFormComponent {}
