01c2f7cf5e97108ff5bc7ec043278ec04acc6908
Alben Bustamante <albenbustamante@gmail.com>
2023-06-02 19:37:34 -0500

    add: a basic table for show the user categories

--- src/app/modules/category/category.module.ts
+++ src/app/modules/category/category.module.ts
@@ -6,9 +6,10 @@ import { CategoriesComponent } from './pages/categories/categories.component';
 import { NewCategoryComponent } from './components/new-category/new-category.component';
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CategoryTableComponent } from './components/category-table/category-table.component';
 
 @NgModule({
-  declarations: [CategoriesComponent, NewCategoryComponent],
+  declarations: [CategoriesComponent, NewCategoryComponent, CategoryTableComponent],
   imports: [
     CommonModule,
     CategoryRoutingModule,
