659e63bf834a06e87fba8ea68234e916a38108da
Alben Bustamante <albenbustamante@gmail.com>
2023-06-09 16:16:01 -0500

    create a subcomponent for nav links

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './components/layout/layout.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavElementComponent } from './components/nav-element/nav-element.component';
 
 @NgModule({
-  declarations: [LayoutComponent, NavbarComponent],
+  declarations: [LayoutComponent, NavbarComponent, NavElementComponent],
   imports: [CommonModule, LayoutRoutingModule],
 })
 export class LayoutModule {}
