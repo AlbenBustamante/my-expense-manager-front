4cb79845340d7b21a634388b7ffb82059a5d219c
Alben Bustamante <albenbustamante@gmail.com>
2023-06-09 23:44:25 -0500

    add: basic design to show the main stats

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SettingsComponent } from './pages/settings/settings.component';
+import { MainStatsComponent } from './components/main-stats/main-stats.component';
 
 @NgModule({
-  declarations: [DashboardComponent, SettingsComponent],
+  declarations: [DashboardComponent, SettingsComponent, MainStatsComponent],
   imports: [CommonModule, ProfileRoutingModule],
 })
 export class ProfileModule {}
