15a4cdd583987ef033066d77571a9ff1200bccbe
Alben Bustamante <albenbustamante@gmail.com>
2023-06-15 12:32:58 -0500

    update: the reports model

--- src/app/core/models/report.model.ts
+++ src/app/core/models/report.model.ts
@@ -1,6 +1,11 @@
-export interface IReportResponse {
+export interface IMainStatsReportResponse {
   totalCategories: number;
   totalTransactions: number;
   totalExpenses: string;
   totalIncomes: string;
+}
+
+export interface IReportResponse {
+  mainStats: IMainStatsReportResponse;
+  currentBalance: string;
 }
--- src/app/modules/profile/components/main-stats/main-stats.component.ts
+++ src/app/modules/profile/components/main-stats/main-stats.component.ts
@@ -8,14 +8,17 @@ import { UserService } from 'src/app/core/services/user.service';
   styleUrls: ['./main-stats.component.css'],
 })
 export class MainStatsComponent {
-  public stats: IReportResponse = {
-    totalCategories: 0,
-    totalTransactions: 0,
-    totalExpenses: '$ 0',
-    totalIncomes: '$ 0',
+  public report: IReportResponse = {
+    mainStats: {
+      totalCategories: 0,
+      totalTransactions: 0,
+      totalExpenses: '0',
+      totalIncomes: '0',
+    },
+    currentBalance: '0',
   };
 
   constructor(private readonly userService: UserService) {
-    this.userService.getReports().subscribe((result) => (this.stats = result));
+    this.userService.getReports().subscribe((result) => (this.report = result));
   }
 }
