0dfbc61dc40580d02b4913077af5d0c1e3cc833e
Alben Bustamante <albenbustamante@gmail.com>
2023-06-19 14:45:53 -0500

    feature: the interceptor removes the token if the token is expired or not valid

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -9,10 +9,14 @@ import {
 import { Observable, catchError, throwError } from 'rxjs';
 import { TokenService } from '../services/token.service';
 import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private tokenService: TokenService) {}
+  constructor(
+    private readonly tokenService: TokenService,
+    private readonly router: Router
+  ) {}
 
   intercept(
     request: HttpRequest<unknown>,
@@ -30,20 +34,15 @@ export class AuthInterceptor implements HttpInterceptor {
 
     return next.handle(authRequest).pipe(
       catchError((error: HttpErrorResponse) => {
-        if (error.status === 403) {
+        if (error.status === 403 || error.status === 401) {
           Swal.fire({
             title: `Oops...`,
             text: `You don't have permissions.`,
             icon: 'error',
           });
-        }
 
-        if (error.status === 401) {
-          Swal.fire({
-            title: 'You are not logged',
-            text: 'Please, login and try again :)',
-            icon: 'error',
-          });
+          this.tokenService.delete();
+          this.router.navigateByUrl('/auth/login');
         }
 
         return throwError(() => error);
--- src/app/core/services/token.service.ts
+++ src/app/core/services/token.service.ts
@@ -1,24 +1,30 @@
 import { Injectable } from '@angular/core';
 import jwtDecode from 'jwt-decode';
-import { setCookie, getCookie } from 'typescript-cookie';
+import { setCookie, getCookie, removeCookie } from 'typescript-cookie';
 import { IJwtResponse } from '../models/auth.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TokenService {
+  private readonly cookie: string = 'token';
+
   constructor() {}
 
   public save(token: string): void {
-    setCookie('token', token, { expires: 1, path: '/' });
+    setCookie(this.cookie, token, { expires: 1, path: '/' });
   }
 
   public get(): string | undefined {
-    return getCookie('token');
+    return getCookie(this.cookie);
+  }
+
+  public delete(): void {
+    removeCookie(this.cookie);
   }
 
   public getInfo(): IJwtResponse {
-    const token = getCookie('token');
+    const token = getCookie(this.cookie);
     return jwtDecode(token ?? '');
   }
 }
